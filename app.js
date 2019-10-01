5d0f3aabebd4aca5bb155a0419443d6374b34645
Cherise <cherisetan@live.com>
2019-10-01 14:49:48 +1300

    Set up public and views directories

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-    res.send("Hello");
+    res.render("index");
 });
 
 app.listen(3000, function() {
